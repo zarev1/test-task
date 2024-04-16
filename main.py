cd8b36a54e60c22cd0878b022567b7719c064363
sirar <saba200@yandex.ru>
2024-04-16 21:27:33 +0400

    Start

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = "https://www.wildberries.ru/catalog/0/search.aspx?search=msi"
 
-print ("hello world")
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+}
+req = requests.get(url, headers=headers)
+src = req.text
+print(src)
