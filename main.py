9045eb96e4462720aae0b289e672955ca358634c
sirar <saba200@yandex.ru>
2024-04-30 16:41:00 +0400

    bot_body

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
+import Config
+import logging
 
-url = "https://www.wildberries.ru/catalog/0/search.aspx?search=msi"
+from aiogram import Bot, Dispatcher, executor,types
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
-}
-req = requests.get(url, headers=headers)
-src = req.text
-print(src)
 
-with open("index.html", "w", encoding="utf-8") as file:
-    file.write(src)
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=Config.TOKEN)
+dp = Dispatcher(bot)
+
+db = Sqligther('db.db')
+
+@dp.message_handler(commands=['subscribe'])
+async  def subscribe(message: types.Message):
+    if(not db.subscriber(message.from_user.id)):
+        db.add_subscriber(message.from_user.id)
+    else:
+        db.update_subscription(message.from_user.id,True)
+    await message.answer("Вы успешно подписались на рассылку!\nЖдите, скоро выйдут новые обзоры и вы узнаете о них!")
+
+@dp.message_handler(commands=['unsubscibe'])
+async def unsubscribe(message: types.Message):
+    if(not db.subscriber_exists(message.from_user.id)):
+        db.add_subscriber(message.from_user.id,False)
+        await message.answer("Вы итак не подписаны")
+    else:
+        db.update_subscriptions(message.from_user.id,False)
+        await message.answer("Вы успешно отписаны от рассылки")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
